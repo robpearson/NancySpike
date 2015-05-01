9915952faf3755bd1ed13e46afe94fa167a0f404
Rob Pearson <rob@maplepixel.com.au>
2015-05-01 12:39:09 +1000

    Updated to add missing.  Should amend last commit but lazy.

---
+++ RobPearson.NancySpike/RobPearson.NancySpike.Test/TransitTimetableModuleTest.cs
@@ -0,0 +1,24 @@
+﻿using Nancy;
+using Nancy.Testing;
+using Xunit;
+
+namespace RobPearson.NancySpike.Test
+{
+    public class TransitTimetableModuleTest
+    {
+        [Fact]
+        public void Should_return_status_ok_when_route_exists()
+        {
+            // Given
+            var bootstrapper = new DefaultNancyBootstrapper();
+            var browser = new Browser(bootstrapper);
+
+            // When
+            var result = browser.Get("/transit/timetable", with => { with.HttpRequest(); });
+
+            // Then
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+//            Assert.Equal("", result.Body);
+        }
+    }
+}
---
+++ RobPearson.NancySpike/RobPearson.NancySpike/TransitTimetableModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace RobPearson.NancySpike
+{
+    /// <summary>
+    /// Transit Timetable Module
+    /// https://api.maplepixel.com.au/transit/timetable
+    /// </summary>
+    public class TransitTimetableModule : NancyModule
+    {
+        public TransitTimetableModule()
+    {
+        // would capture routes to /products/list sent as a GET request
+        Get["/transit/timetable"] = parameters => {
+            return "All Transit Times for today";
+        };
+    }
+    }
+}
