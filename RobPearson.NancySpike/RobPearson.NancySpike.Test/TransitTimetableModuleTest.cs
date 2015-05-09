2e25a35b8663934fb896514336bd084565d2e0c1
Rob Pearson <rob@maplepixel.com.au>
2015-05-09 22:37:25 +1000

    Updated to finish get test and added initial post test.

--- RobPearson.NancySpike/RobPearson.NancySpike.Test/TransitTimetableModuleTest.cs
+++ RobPearson.NancySpike/RobPearson.NancySpike.Test/TransitTimetableModuleTest.cs
@@ -10,13 +10,13 @@ namespace RobPearson.NancySpike.Test
     public class TransitTimetableModuleTest
     {
         [Fact]
-        public void Should_return_status_ok_when_route_exists()
+        public void Can_Get_Request()
         {
             // Given
             var browser = new Browser(with => with.Module(new TransitTimetableModule()));
 
             // When
-            var response = browser.Get("/transit/timetable/2015-05-02",
+            var response = browser.Get("/transit/timetable/2015-05-2",
                 with =>
                 {
                     with.HttpRequest();
@@ -28,9 +28,57 @@ namespace RobPearson.NancySpike.Test
 
             var trips = response.Body.DeserializeJson<IEnumerable<TransitTripDetail>>().ToList();
             Assert.Equal(2, trips.Count());
-            Assert.Equal("CAB", trips.ElementAt(0).RouteShortName);
-            Assert.Equal("Caboolture Line", trips.ElementAt(0).RouteLongName);
-            // TODO: Add more asserts
+            Assert.Equal("CAB", trips[0].RouteShortName);
+            Assert.Equal("Caboolture Line", trips[0].RouteLongName);
+            Assert.Equal("Bald Hills", trips[0].DepartingStopName);
+            Assert.Equal("Platform 1", trips[0].DepartingPlatform);
+            Assert.Equal(new DateTime(2015, 05, 02, 08, 02, 00).ToString("s"), trips[0].DepartingTime.ToString("s"));
+            Assert.Equal("12345.6, 5555.6", trips[0].DepartingLocation);
+            Assert.Equal("Central", trips[0].ArrivingStopName);
+            Assert.Equal("Platform 5", trips[0].ArrivingPlatform);
+            Assert.Equal(new DateTime(2015, 05, 02, 08, 34, 00).ToString("s"), trips[0].ArrivingTime.ToString("s"));
+            Assert.Equal("1235.77, 98171.1", trips[0].ArrivingLocation);
+
+            Assert.Equal("CAB", trips[1].RouteShortName);
+            Assert.Equal("Caboolture Line", trips[1].RouteLongName);
+            Assert.Equal("Bald Hills", trips[1].DepartingStopName);
+            Assert.Equal("Platform 1", trips[1].DepartingPlatform);
+            Assert.Equal(new DateTime(2015, 05, 02, 08, 14, 00).ToString("s"), trips[1].DepartingTime.ToString("s"));
+            Assert.Equal("12345.6, 5555.6", trips[1].DepartingLocation);
+            Assert.Equal("Central", trips[1].ArrivingStopName);
+            Assert.Equal("Platform 5", trips[1].ArrivingPlatform);
+            Assert.Equal(new DateTime(2015, 05, 02, 08, 47, 00).ToString("s"), trips[1].ArrivingTime.ToString("s"));
+            Assert.Equal("1235.77, 98171.1", trips[1].ArrivingLocation);
+        }
+
+        [Fact]
+        public void Can_Post_Request()
+        {
+            // Given
+            var trip = new FavouriteTrip
+            {
+                TripId = Guid.NewGuid().ToString(),
+                DepartingStopId = "1234",
+                DepartingChildStopIds = "221, 224, 225",
+                DepartingStopName = "Bald Hills",
+                DepartingLocation = "12345.6, 5555.6",
+                ArrivingStopId = "4533",
+                ArrivingChildStopIds = "551, 552, 553, 554, 555, 556",
+                ArrivingLocation = "1235.77, 98171.1",
+                ArrivingStopName = "Central"
+            };
+            var browser = new Browser(with => with.Module(new TransitTimetableModule()));
+
+            // When
+            var response = browser.Post("/transit/FavouriteTrip",
+                with =>
+                {
+                    with.HttpRequest();
+                    with.JsonBody(trip);
+                });
+
+            // Then
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);   
         }
     }
 }
--- RobPearson.NancySpike/RobPearson.NancySpike/TransitTimetableModule.cs
+++ RobPearson.NancySpike/RobPearson.NancySpike/TransitTimetableModule.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using Nancy;
+using Nancy.ModelBinding;
 
 namespace RobPearson.NancySpike
 {
     /// <summary>
-    ///     Transit Timetable Module
-    ///     https://api.maplepixel.com.au/transit/timetable
+    ///     Transit Module
+    ///     https://api.maplepixel.com.au/transit/
     /// </summary>
     public class TransitTimetableModule : NancyModule
     {
         public TransitTimetableModule()
         {
             Get["/transit/timetable/{timestamp}"] = p => GetTransitTimeTable(p.timestamp);
+
+            Post["/transit/FavouriteTrip"] = p =>
+            {
+                // TODO: This should probably be in something like a settings module
+                var trip = this.Bind<FavouriteTrip>();
+
+                // TODO: Repo should be injected
+                var settingsRepo = new SettingsRepository();
+                var id = settingsRepo.SaveFavouriteTrip(trip);
+
+                // TODO: Add url to response for new resource
+                return HttpStatusCode.OK;
+            };
         }
 
         private IEnumerable<TransitTripDetail> GetTransitTimeTable(DateTime timestamp)
@@ -29,7 +43,7 @@ namespace RobPearson.NancySpike
                 DepartingLocation = "12345.6, 5555.6",
                 ArrivingStopName = "Central",
                 ArrivingPlatform = "Platform 5",
-                ArrivingTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 08, 34, 00),
+                ArrivingTime = new DateTime(timestamp.Year, timestamp.Month, timestamp.Day, 08, 34, 00),
                 ArrivingLocation = "1235.77, 98171.1"
             };
 
@@ -39,11 +53,11 @@ namespace RobPearson.NancySpike
                 RouteLongName = "Caboolture Line",
                 DepartingStopName = "Bald Hills",
                 DepartingPlatform = "Platform 1",
-                DepartingTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 08, 14, 00),
+                DepartingTime = new DateTime(timestamp.Year, timestamp.Month, timestamp.Day, 08, 14, 00),
                 DepartingLocation = "12345.6, 5555.6",
                 ArrivingStopName = "Central",
                 ArrivingPlatform = "Platform 5",
-                ArrivingTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 08, 47, 00),
+                ArrivingTime = new DateTime(timestamp.Year, timestamp.Month, timestamp.Day, 08, 47, 00),
                 ArrivingLocation = "1235.77, 98171.1"
             };
 
@@ -52,6 +66,33 @@ namespace RobPearson.NancySpike
 
             return timetable;
         }
+    }
+
+    public interface ISettingsRepository
+    {
+        int SaveFavouriteTrip(FavouriteTrip trip);
+    }
+
+    public class SettingsRepository : ISettingsRepository
+    {
+        public int SaveFavouriteTrip(FavouriteTrip trip)
+        {
+            // Do nothing for now
+            return 1;
+        }
+    }
+
+    public class FavouriteTrip
+    {
+        public string TripId { get; set; }
+        public string DepartingStopId { get; set; }
+        public string DepartingChildStopIds { get; set; }
+        public string DepartingStopName { get; set; }
+        public string DepartingLocation { get; set; }
+        public string ArrivingStopId { get; set; }
+        public string ArrivingChildStopIds { get; set; }
+        public string ArrivingLocation { get; set; }
+        public string ArrivingStopName { get; set; }
     }
 
     public class TransitTripDetail
